111bc9478298186102d62a6e19b1f2aa14661512
Jaime Silva <jaimesilvarod@hotmail.com>
2024-06-07 11:56:23 -0500

    Aplicación de Abstracción en Sistema de Pago, no añadimos Interfaces porque no se pueden crear funciones.

---
+++ src/OnlineStore/PagoTransferenciaBancaria.java
@@ -0,0 +1,20 @@
+package OnlineStore;
+
+public class PagoTransferenciaBancaria extends SistemadePago{
+
+
+    @Override
+    public void iniciarPago() {
+        System.out.println("Iniciando Pago");
+    }
+
+    @Override
+    public void verificarPago() {
+        System.out.println("Verificando Pago");
+    }
+
+    @Override
+    public void confirmarPago() {
+        System.out.println("Pago confirmado");
+    }
+}
---
+++ src/OnlineStore/SistemadePago.java
@@ -0,0 +1,9 @@
+package OnlineStore;
+
+public abstract class SistemadePago {
+
+    public abstract void iniciarPago();
+    public abstract void verificarPago();
+    public abstract void confirmarPago();
+
+}
